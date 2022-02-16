f04c66490c5d26547b99d3cce1ae867f04015439
Seokhyeon-Park <dfbgq346q@gmail.com>
2022-02-16 17:30:31 +0900

    *

--- test.js
+++ test.js
@@ -1,6 +1,36 @@
 "use strict"
 
+const TEST = {
+    dataSet : new Object(),
+
+    init : new function(){},
+    gridSet : new function(){},
+}
+
+TEST.init = () => {
+    TEST.gridSet();
+}
+
+TEST.gridSet = () => {
+    // 임시 테스트 데이터
+    TEST.dataSet = [
+        {
+            'name' : '석봉',
+            'year' : '1994',
+            'date' : '0905',
+            'region' : '부천',
+        },
+        {
+            'name' : '시봉',
+            'year' : '1994',
+            'date' : '1231',
+            'region' : '충주',
+            'etc' : '바보임',
+        }
+    ];
+}
+
 // 실행
 $(function(){
-    
+    TEST.init();
 })
