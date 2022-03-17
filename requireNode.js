9c708a4bd6723f4d525453b84e31cc8b499f4e8c
seokhyeon Park <dfbgq346q@gmail.com>
2022-03-17 17:25:02 +0900

    *

---
+++ requireNode.js
@@ -0,0 +1,8 @@
+// https://digiconfactory.tistory.com/entry/Nodejs-%EB%AA%A8%EB%93%88%EC%82%AC%EC%9A%A9%EB%B2%95?category=872629
+const exprotsNode = require('./exprotsNode.js');
+ 
+ 
+console.log('add:'+exprotsNode.add(3,5));
+console.log('sub:'+exprotsNode.sub(7,3));
+console.log('mul:'+exprotsNode.mul(7,7));
+console.log('div:'+exprotsNode.div(9,3));
