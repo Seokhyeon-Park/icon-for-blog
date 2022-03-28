0cb4abe3f9f9a08b2dc224b7cbdc3abc0cd46e06
seokhyeon Park <dfbgq346q@gmail.com>
2022-03-28 15:23:22 +0900

    *

--- test.js
+++ test.js
@@ -3,7 +3,18 @@
 
 // 실행
 $(function(){
-    $(document).on('click', '#testBtn', function(){
-        $('#testDiv').text(("gm3259239592390589203582385").match(/[0-9]{0,13}$/));
+    $(document).on('click', '#testBtn1', function(){
+        const myAttr = $("#testDiv").attr("class");
+        console.log("class1 ? : ", myAttr);
+    });
+
+    $(document).on('click', '#testBtn2', function(){
+        $("#testDiv").attr("class", "addClass");
+        console.log("class2 ? : ", $("#testDiv").attr("class"));
+    });
+
+    $(document).on('click', '#testBtn3', function(){
+        $("#testDiv").removeAttr("class");
+        console.log("class3 ? : ", $("#testDiv").attr("class"));
     });
 })
